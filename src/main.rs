7133ae3b0d6b056818be531e72a252515aa6a970
ddordain <ddordain@student.42.fr>
2023-05-14 18:53:41 +0200

    [UPDATE] dependencies fix for r2d2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
+    // load the env var
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let app = blog_actix::Blog::new(8998);
+    app.run(database_url)
+   
 }
