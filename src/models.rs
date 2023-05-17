9413f6d9a4def2ed6bde7e5e1c1a9035b92de194
ddordain <ddordain@student.42.fr>
2023-05-18 00:07:40 +0200

    [update] new functions for creating and retrieving post

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,8 @@
 use crate::errors::AppError;
-use crate::schema::{users};
+use crate::schema::users;
 use diesel::prelude::*;
 type Result<T> = std::result::Result<T, AppError>;
+use crate::schema::posts;
 
 #[derive(Queryable, Identifiable, Serialize, Debug, PartialEq)]
 pub struct User {
@@ -9,11 +10,10 @@ pub struct User {
     pub username: String,
 }
 
-
 pub fn create_user(conn: &SqliteConnection, username: &str) -> Result<User> {
     conn.transaction(|| {
         diesel::insert_into(users::table)
-            .values((users::username.eq(username),))       
+            .values((users::username.eq(username),))
             .execute(conn)?;
 
         users::table
@@ -29,7 +29,6 @@ pub enum UserKey<'a> {
     ID(i32),
 }
 
-
 pub fn find_user<'a>(conn: &SqliteConnection, key: UserKey<'a>) -> Result<User> {
     match key {
         UserKey::Username(name) => users::table
@@ -37,10 +36,74 @@ pub fn find_user<'a>(conn: &SqliteConnection, key: UserKey<'a>) -> Result<User> 
             .select((users::id, users::username))
             .first::<User>(conn)
             .map_err(AppError::from),
-        UserKey::ID(id) =>users::table
+        UserKey::ID(id) => users::table
             .find(id)
             .select((users::id, users::username))
             .first::<User>(conn)
             .map_err(Into::into),
     }
+}
+
+// Associations and Identifiable are traits necessary for belongs_to(User)
+#[derive(Queryable, Associations, Identifiable, Serialize, Debug)]
+#[belongs_to(User)]
+pub struct Post {
+    pub id: i32,
+    pub user_id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+pub fn create_post(conn: &SqliteConnection, user: &User, title: &str, body: &str) -> Result<Post> {
+    conn.transaction(|| {
+        diesel::insert_into(posts::table)
+            .values((
+                posts::user_id.eq(user.id),
+                posts::title.eq(title),
+                posts::body.eq(body),
+            ))
+            .execute(conn)?;
+
+        posts::table
+            .order(posts::id.desc())
+            .select(posts::all_columns)
+            .first(conn)
+            .map_err(Into::into)
+    })
+}
+
+pub fn publish_post(conn: &SqliteConnection, post_id: i32) -> Result<Post> {
+    conn.transaction(|| {
+        diesel::update(posts::table.filter(posts::id.eq(post_id)))
+            .set(posts::published.eq(true))  
+            .execute(conn)?;
+        
+        posts::table
+            .find(post_id)
+            .select(posts::all_columns)
+            .first(conn)
+            .map_err(Into::into)
+    })
+}
+
+
+pub fn all_posts(conn: &SqliteConnection) -> Result<Vec<(Post, User)>> {
+    posts::table
+        .order(posts::id.desc())
+        .filter(posts::published.eq(true))
+        .inner_join(users::table)
+        .select((posts::all_columns, (users::id, users::username)))
+        .load::<(Post, User)>(conn)
+        .map_err(Into::into)
+}
+
+
+pub fn user_posts(conn: &SqliteConnection, user_id: i32) -> Result<Vec<Post>> {
+    posts::table
+        .filter(posts::user_id.eq(user_id))
+        .order(posts::id.desc())
+        .select(posts::all_columns)
+        .load::<Post>(conn)
+        .map_err(Into::into)
 }
